c2d32154dc69e2f9d711ca35b3cf1e3f66e63e28
EhsanAliverdi <55515740+EhsanAliverdi@users.noreply.github.com>
2025-11-19 12:03:48 +1100

    fixed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // src/App.tsx
 import { useMemo, useState } from "react";
-import { AppLayout } from "./components1/AppLayout";
-import { SidebarSectionNav } from "./components1/SidebarSectionNav";
-import { CheatCard } from "./components1/CheatCard";
-import { CheatModal } from "./components1/CheatModal";
+import { AppLayout } from "./components/AppLayout";
+import { SidebarSectionNav } from "./components/SidebarSectionNav";
+import { CheatCard } from "./components/CheatCard";
+import { CheatModal } from "./components/CheatModal";
 import type { CheatItem, CheatExample } from "./core/cheatsheet-types";
 import { allSections } from "./data/sections";
+
 
 function App() {
   const [selectedSectionId, setSelectedSectionId] = useState<string>(
